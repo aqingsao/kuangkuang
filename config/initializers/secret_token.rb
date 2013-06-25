d0c8ce33b3cb9fe2b608e00e68bdd8b6bb8b6b13
aqingsao <vinci.zhang@gmail.com>
2013-06-25 13:45:10 +0800

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kuangkuang::Application.config.secret_token = '1d8d2d8831e7581a8f8b29e4784a0abbe6ecd6e912d6a69bc5ea90df3889db190d80b0794a91a1f199c50a0d43921af46c390de7540343f908ba713ca8c7c588'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kuangkuang::Application.config.session_store :cookie_store, key: '_kuangkuang_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kuangkuang::Application.config.session_store :active_record_store
