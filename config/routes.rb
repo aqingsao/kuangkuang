eda708e726940465f2dbbe2c3b784144cd15aaed
aqingsao <vinci.zhang@gmail.com>
2013-06-26 13:41:21 +0800

    create product resource

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Kuangkuang::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :products
 
   # Sample resource route with options:
   # resources :products do
