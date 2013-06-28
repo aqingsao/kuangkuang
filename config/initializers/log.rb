0dbbf4d778ac6c1fe999ef0422e80cda55a2f5e4
aqingsao <vinci.zhang@gmail.com>
2013-06-28 13:49:33 +0800

    move config from log.rb to development.rb

--- config/initializers/log.rb
+++
@@ -1,4 +0,0 @@
-Kuangkuang::Application.config.log_level = :warn
-Kuangkuang::Application.config.log_tags = [
-	lambda { |req| Time.now }, 
-	lambda{|req| "#{req.cookie_jar["_kuangkuang_session"]}"[0..31]}]
