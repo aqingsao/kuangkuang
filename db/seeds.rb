fa09ed2f8b8d2f9799a80a2d52f51f279089f6f9
aqingsao <vinci.zhang@gmail.com>
2013-07-05 16:27:36 +0800

    add more products

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-p1 = Product.create(:name=>'IPad 3', :price=>3888.00)
-p2 = Product.create(:name=>'Sony Digital Camera', :price=>1999.00)
-p3 = Product.create(:name=>'iPone 5', :price=>4666.00)
-p4 = Product.create(:name=>'MacBook Pro', :price=>9999.00)
-p5 = Product.create(:name=>'MacBook Air', :price=>12888.00)
+@@productIndex = 0;
+def productId
+	sprintf("34%03d%02d", rand(1000), @@productIndex += 1).to_i
+end
 
+100.times do |i|
+	p = Product.new(:name=>"product #{i}", :price=>rand(10000).to_f)
+	p.id = productId
+	p.save!
+end
+# p1 = Product.new(:name=>'IPad 3', :price=>3888.00)
+# p2 = Product.new(:name=>'Sony Digital Camera', :price=>1999.00)
+# p3 = Product.new(:name=>'iPone 5', :price=>4666.00)
+# p4 = Product.new(:name=>'MacBook Pro', :price=>9999.00)
+# p5 = Product.new(:name=>'MacBook Air', :price=>12888.00)
